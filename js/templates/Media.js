6d5ea4c5c5e7118351413a7366c0d2100c680f41
michael-lmk <>
2022-01-05 18:18:01 +0100

    Creation Media class

---
+++ js/templates/Media.js
@@ -0,0 +1,15 @@
+class Media {
+    constructor(data){
+        this._id;
+        this._photographerId = data.photographerId;
+        this._title = data.title;
+        this._image = data.image;
+        this._tags = data.tags;
+        this._likes = data.likes;
+        this._date = data.date;
+        this._price = data.price;
+        this._alt_text = data.alt_text;
+    }
+
+    
+}
